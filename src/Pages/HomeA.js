7a69b330a76e688a36b3e73a90896703b7de2780
ZyzzAnas <anass.fix12@gmail.com>
2021-04-18 01:31:00 +0100

    autoComplete

--- src/Pages/HomeA.js
+++ src/Pages/HomeA.js
@@ -1,4 +1,5 @@
 import React from "react";
+import SearchBar from "./SearchBar.js"
 
 function HomeA() {
 
@@ -8,8 +9,9 @@ function HomeA() {
         <div className="aa-slider-area">
           <div className="aa-top-slider">
           <div className="aa-top-slider-single">
-            <div id="img-hero"></div>
-            <div className="aa-top-slider-content"><span className="aa-top-slider-catg">Duplex</span>
+            <div id="img-hero">
+
+             <SearchBar></SearchBar>
              
             </div>
           </div>
@@ -18,75 +20,7 @@ function HomeA() {
         </div>
       </section>
 
-      <section id="aa-advance-search">
-        <div className="container">
-          <div className="aa-advance-search-area">
-            <div className="form">
-              <div className="aa-advance-search-top">
-                <div className="row">
-                  <div className="col-md-4">
-                    <div className="aa-single-advance-search">
-                      <input type="text" placeholder="Type Your Location"/>
-                    </div>
-                  </div>
-                  <div className="col-md-2">
-                    <div className="aa-single-advance-search">
-                      <select>
-                        <option value="0" selected>Category</option>
-                        <option value="1">Flat</option>
-                        <option value="2">Land</option>
-                        <option value="3">Plot</option>
-                        <option value="4">Commercial</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className="col-md-2">
-                    <div className="aa-single-advance-search">
-                      <select>
-                        <option value="0" selected>Type</option>
-                        <option value="1">Flat</option>
-                        <option value="2">Land</option>
-                        <option value="3">Plot</option>
-                        <option value="4">Commercial</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className="col-md-2">
-                    <div className="aa-single-advance-search">
-                      <select>
-                        <option value="0" selected>Type</option>
-                        <option value="1">Flat</option>
-                        <option value="2">Land</option>
-                        <option value="3">Plot</option>
-                        <option value="4">Commercial</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className="col-md-2">
-                    <div className="aa-single-advance-search">
-                      <input className="aa-search-btn" type="submit" value="Search"/>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="aa-advance-search-bottom">
-                <div className="row">
-                  <div className="col-md-6">
-                    <div className="aa-single-filter-search"><span>AREA (SQ)</span> <span>FROM</span> <span id="skip-value-lower" className="example-val">30.00</span> <span>TO</span> <span id="skip-value-upper" className="example-val">100.00</span>
-                      <div id="aa-sqrfeet-range" className="noUi-target noUi-ltr noUi-horizontal noUi-background"></div>
-                    </div>
-                  </div>
-                  <div className="col-md-6">
-                    <div className="aa-single-filter-search"><span>PRICE ($)</span> <span>FROM</span> <span id="skip-value-lower2" className="example-val">30.00</span> <span>TO</span> <span id="skip-value-upper2" className="example-val">100.00</span>
-                      <div id="aa-price-range" className="noUi-target noUi-ltr noUi-horizontal noUi-background"></div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+
       <section id="aa-about-us">
         <div className="container">
           <div className="row">
