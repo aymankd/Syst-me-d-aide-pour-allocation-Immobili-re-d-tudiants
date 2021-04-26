cc4c949842fbaedda116eb6cb16b63b7b835216a
jora <nabilbakour0@gmail.com>
2021-04-26 23:53:52 +0000

    compItem

---
+++ Client/src/Pages/Copmitem.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function Copmitem() {
+
+  return (
+
+    <div>
+    <div className="col-md-4">
+                              <article className="aa-properties-item"><a
+                                      href="/"
+                                      className="aa-properties-item-img"><img src="assets/img/item/1.jpg"
+                                          alt="website template image1" /></a>
+                                  <div className="aa-tag for-sale">For Rent</div>
+                                  <div className="aa-properties-item-content">
+                                      <div className="aa-properties-info"><span><i class="fas fa-door-open"></i>5 Rooms</span> <span><i class="fa fa-bed" aria-hidden="true"></i> 2 Beds</span> <span>3 Baths</span><span>1100 SQ FT</span></div>
+                                      <div className="aa-properties-about">
+                                          <h3><a href="/">Appartment Title</a>
+                                          </h3>
+                                          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim molestiae vero
+                                              ducimus quibusdam odit vitae.</p>
+                                      </div>
+                                      <div className="aa-properties-detial"><span className="aa-price"> 3500 Dh</span> <a
+                                              href="/"
+                                              className="aa-secondary-btn">View Details</a></div>
+                                  </div>
+                              </article>
+                          </div>
+    </div>
+  )
+}
+export default Copmitem;
--- Client/src/Pages/ResultPage.js
+++ Client/src/Pages/ResultPage.js
@@ -3,7 +3,9 @@ import React from "react";
 function ResultPage() {
 
   return (
-    <div></div>
+    <div>
+        
+    </div>
   )
 }
 export default ResultPage;
