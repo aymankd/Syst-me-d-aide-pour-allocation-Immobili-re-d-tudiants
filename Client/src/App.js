eed687794ec4fe7d13ed926772c1fb29abd8420d
jora <nabilbakour0@gmail.com>
2021-05-03 00:57:18 +0000

    see more page

--- Client/src/App.js
+++ Client/src/App.js
@@ -11,7 +11,7 @@ import NavBare from './Pages/NavBare';
 import HomeA from './Pages/HomeA';
 import ResultPage from './Pages/ResultPage';
 import Copmitem from './Pages/Copmitem';
-
+import seemore from './Pages/seemore';
 function App() {
   return (
     <div className="App">
@@ -19,7 +19,8 @@ function App() {
           <NavBare/>
           <Route exact path="/" component = {HomeA} />
           <Route exact path="/ResultPage" component = {ResultPage} /> 
-          <Route exact path="/Copmitem" component = {Copmitem} /> 
+          <Route exact path="/Copmitem" component = {Copmitem} />
+          <Route exact path="/seemore" component = {seemore} /> 
         </Router>
     </div>
   );
